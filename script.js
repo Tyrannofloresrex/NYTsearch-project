571a335898659761cd58d338c23c64a3eec55163
Kevin Lyons <zinckev@hotmail.com>
2020-12-18 10:46:14 -0800

    add comments for api search

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("this is linked");
+
+// how to search on the API /articlesearch.json?q={query}&fq={filter} (query and filter searches must start capital)
+// api url https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+// our api key XDYe3JhDujiJEfHHkmrijdblWWqX58GH
