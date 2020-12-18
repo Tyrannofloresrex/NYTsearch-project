23de20a8e746acef1bd1f4e66021a955a77bc77a
Kevin Lyons <zinckev@hotmail.com>
2020-12-18 11:49:05 -0800

    uploading js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 console.log("this is linked");
 
-// how to search on the API /articlesearch.json?q={query}&fq={filter} (query and filter searches must start capital)
-// api url https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
-// our api key XDYe3JhDujiJEfHHkmrijdblWWqX58GH
+// variables
+var apiKey = "XDYe3JhDujiJEfHHkmrijdblWWqX58GH";
+var search = $("#searchTerm");
+var start = $("#startYear");
+var end = $("#endYear");
+var results = $("resultNum");
+
+var queryURL =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=" +
+  search +
+  "&begin_date=" +
+  start +
+  "&end_date=" +
+  end +
+  "&api-key=" +
+  apiKey +
+  "=" +
+  results;
+
+// Calvin-Work =======================
+
+$("#searchButton").click(function (event) {
+  $.get(queryURL).then(
+    function (response) {
+      console.log(`articles: ${response}`);
+    } // var articlesList =
+  );
+});
